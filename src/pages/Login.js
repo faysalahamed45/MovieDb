cce77ded604cd8ca1922e2ca636b45843e04b0f6
faysalahamed45 <faysalahamed306@gmail.com>
2025-05-09 10:31:48 +0100

    dark toggle fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,8 +22,8 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-netflix-background text-white">
-      <form onSubmit={handleSubmit} className="bg-black p-6 rounded shadow w-full max-w-md">
+    <div className="flex items-center justify-center min-h-screen bg-netflix-background text-white bg-white dark:bg-gray-900 text-black dark:text-white ">
+      <form onSubmit={handleSubmit} className="bg-zinc-100 dark:bg-black p-6 rounded border dark:border-none shadow w-full max-w-md">
         <h2 className="text-2xl font-bold mb-4 text-netflix-red">Login</h2>
         <input type="text" name="username" placeholder="Username" className="w-full p-2 mb-3 bg-gray-800 text-white border rounded" onChange={handleChange} />
         <input type="password" name="password" placeholder="Password" className="w-full p-2 mb-3 bg-gray-800 text-white border rounded" onChange={handleChange} />
